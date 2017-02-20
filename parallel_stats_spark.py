2adb7c7a43a7793f2fa11e9eb9fe02bd387958ad
Sean Olejar <solejar236@gmail.com>
2017-02-20 12:44:09 -0500

    this is all input files + script which makes them

---
+++ parallel_stats_spark.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+for i in range(1,6):
+    
+    outputName = "input_file_10^{0}.txt".format(i)
+    file = open(outputName,'w')
+    print("writing to file ",outputName, "with i of ", str(i))
+    for j in range(1,pow(10,i)+1):
+        #print(str(pow(10,i))+1))
+        num = randint(-2222222222222,2222222222222)
+        file.write(str(num)+'\n')
+
+
